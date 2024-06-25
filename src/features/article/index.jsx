1af9a6585813f8cc174a2f896cc3d9a62671bc75
Armen <armenjan2310@gmail.com>
2024-06-25 17:19:31 +0400

    add: responsivation project

--- src/features/article/index.jsx
+++ src/features/article/index.jsx
@@ -2,14 +2,14 @@ import ProfileInfo from "./profile-info/index";
 
 export default function Article() {
   return (
-    <article className="w-full h-full flex justify-center items-center">
-      <section className="w-[80%] h-[80%] bg-gray-500/20 rounded-[2rem] flex items-center justify-center gap-6 p-6">
+    <article className="w-full h-full flex justify-center items-center max-sm:p-[10px]">
+      <section className="w-full sm:w-[80%] min-h-[90%] bg-gray-500/20 rounded-[2rem] max-md:flex-col md:items-center md:justify-center gap-6 p-6 flex">
         <ProfileInfo />
-        <div className="w-[50%] h-full flex flex-col gap-6">
-          <div className="w-full bg-white h-[50%] rounded-[2rem] flex justify-center items-center">
+        <div className="w-full md:w-[50%] h-full flex flex-col gap-6">
+          <div className="w-full h-[300px] bg-white rounded-[2rem] flex justify-center items-center">
             Info...
           </div>
-          <div className="w-full bg-white h-[50%] rounded-[2rem] flex justify-center items-center">
+          <div className="w-full h-[300px] bg-white rounded-[2rem] flex justify-center items-center">
             Info...
           </div>
         </div>
--- src/features/article/profile-info/index.jsx
+++ src/features/article/profile-info/index.jsx
@@ -1,16 +1,16 @@
 export default function ProfileInfo() {
   return (
-    <div className="w-[450px] h-[530px] bg-white rounded-[2rem] p-[30px] flex flex-col justify-between gap-y-5">
-      <div>
+    <div className="w-full md:w-[450px] max-h-max bg-white rounded-[2rem] max-xs:p-[5px]  p-[30px] max-sm:flex-col flex md:flex-col justify-between gap-5">
+      <div className="sm:w-[90%] md:w-full h-full flex items-center">
         <img
           className="rounded-[2rem]"
           src="/media/images/avatar.jpg"
           alt="avatar"
         />
       </div>
-      <div className="w-full h-[50%] flex flex-col gap-2">
-        <h3 className="text-2xl font-medium">My profile</h3>
-        <div className="flex gap-6 font-medium text-lg">
+      <div className="w-full h-[50%] flex flex-col gap-6">
+        <h3 className="sm:text-2xl font-medium text-xl">My profile</h3>
+        <div className="flex gap-6 font-medium sm:text-lg text-md max-xs:text-xs">
           <div className="w-[50%] h-[40px] border-b-[0.5px] border-gray-500 flex items-center">
             Simon Stake
           </div>
@@ -18,14 +18,14 @@ export default function ProfileInfo() {
             +374 88 77 66 55
           </div>
         </div>
-        <div className="w-full h-[40px] border-b-[0.5px] border-gray-500 flex items-center font-medium text-lg">
+        <div className="w-full h-[40px] border-b-[0.5px] border-gray-500 flex items-center font-medium sm:text-lg text-md max-xs:text-xs">
           s.menchoilik@gmail.com
         </div>
-      </div>
-      <div className="w-full flex justify-center">
-        <button className="w-[90px] h-[40px] text-2xl rounded-[2rem] bg-green-400 p-4 flex justify-center items-center">
-          Save
-        </button>
+        <div className="w-full flex justify-center ">
+          <button className="w-[70px] h-[30px] sm:w-[90px] sm:h-[40px] max-xs:text-xs text-lg sm:text-2xl rounded-[2rem] bg-green-400 p-4 flex justify-center items-center">
+            Save
+          </button>
+        </div>
       </div>
     </div>
   );
